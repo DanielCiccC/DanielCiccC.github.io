5e4acb784a145a4690258c9a22e29ee5c3ad851b
Daniel Ciccotosto-Camp <DanielCiccotostoCamp@nti.com.au>
2023-07-30 13:37:22 +1000

    Modified documents

--- build.sh
+++ build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-# Set the source and destination directories
-sourceDirectory="BSAN4201/notes"
-destinationDirectory="BSAN4201/docs"
+# Redirect debug output and errors to errorlog.txt
+exec > errorlog.txt 2>&1
 
-# Create the destination directory if it doesn't exist
-# mkdir -p "$destinationDirectory"
+# Set the root directory of your course subjects
+root_directory="C:/git/DanielCiccC.github.io"
 
-# # Copy all HTML files from the source to the destination directory
-# cp "$sourceDirectory"/*.html "$destinationDirectory"
+# Loop through all the course subjects
+for course in "$root_directory"/*; do
+    # Check if the 'notes' and 'docs' directories exist for the current course
+    if [ -d "$course/notes/assets" ] && [ -d "$course/docs/assets" ]; then
+        # Copy images from 'notes/assets' to 'docs/assets'
+        cp "$course/notes/assets"/*.{jpg,jpeg,PNG,png,gif} "$course/docs/assets/"
+    else
+        echo "Skipping $course: 'notes' or 'docs' directory not found."
+    fi
+done
 
 # echo "All HTML documents have been copied from $sourceDirectory to $destinationDirectory."
 git add .
