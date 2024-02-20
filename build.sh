cab1795f19fa758b00958c62474211de40455974
Daniel Ciccotosto-Camp <DanielCiccotostoCamp@nti.com.au>
2024-02-20 20:36:39 +1000

    The following have been changed: build.sh errorlog.txt

--- build.sh
+++ build.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Redirect debug output and errors to errorlog.txt
 exec > errorlog.txt 2>&1
 
-# Set the root directory of your course subjects
 root_directory="C:/git/DanielCiccC.github.io"
 
-# Loop through all the course subjects
 for course in "$root_directory"/*; do
-    # Check if the 'notes' and 'docs' directories exist for the current course
     if [ -d "$course/notes/assets" ] && [ -d "$course/docs/assets" ]; then
-        # Copy images from 'notes/assets' to 'docs/assets'
         cp "$course/notes/assets"/*.{jpg,jpeg,PNG,png,gif} "$course/docs/assets/"
     else
         echo "Skipping $course: 'notes' or 'docs' directory not found."
@@ -35,7 +30,7 @@ else
 fi
 
 
-# echo "All HTML documents have been copied from $sourceDirectory to $destinationDirectory."
+# git stuff
 MESSAGE=$(git ls-files -domz | xargs -0 echo The following have been changed: )
 git add .
 git commit -m "$MESSAGE"
